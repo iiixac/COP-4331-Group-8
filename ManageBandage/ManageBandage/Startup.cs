6f94fd375704c58c16f728a945bdc721c4bd1b06
jwa-0030 <2048Donegan!>
2019-01-24 17:07:12 -0500

    Created the front end

---
+++ ManageBandage/ManageBandage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManageBandage.Startup))]
+namespace ManageBandage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
